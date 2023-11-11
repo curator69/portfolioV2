3ec6b49ba9dff8fb243b54bed5dd7c53f56c8202
Rushikesh <rushikeshkhillari10@gmail.com>
2023-11-12 00:08:51 +0530

    init commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import dndeximgroup from "./dndeximgroup.png";
+import host2unlimited from "./host2unlimited.png";
+import weassemble from "./weassemble.png";
+
+export {
+  hero,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  logo,
+  dndeximgroup,
+  host2unlimited,
+  weassemble,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,234 @@
+import {
+  dndeximgroup,
+  host2unlimited,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  weassemble,
+} from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  nestjs,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: nestjs,
+    name: "NestJs",
+    type: "Backend",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Back office admin and Website Developer",
+    company_name: "DND Exim Group",
+    icon: dndeximgroup,
+    iconBg: "white",
+    date: "Feb 2021 - Aug 2021",
+    points: [
+      "Co-ordinating from the pickup of the package to its delivery to its location",
+      "Checking and Maintaining the quality and integrity of the packaging",
+      "Co-ordinating with international clients in various countries",
+      "Updating the company's website with the latest data, blogs, etc as well updating the ui of the website.",
+    ],
+  },
+  {
+    title: "ReactJs Intern",
+    company_name: "Host2unlimited",
+    icon: host2unlimited,
+    iconBg: "white",
+    date: "Sep 2022 - Nov 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Frontend Developer",
+    company_name: "WeAssemble",
+    icon: weassemble,
+    iconBg: "white",
+    date: "Nov 2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/curator69",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/rushikesh-k-3a3977153/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "E-commerce Website",
+    description:
+      "E-commerce website to find your electronics at the best rate possible.",
+    demo_link: "https://ecommerce-curator.vercel.app/",
+    github_link: "https://github.com/curator69/ecommerce-curator",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Meals",
+    description:
+      "A website to find your favorite meal's recipe and ingredients and a youtube video following the same.",
+    demo_link: "https://meals-mu.vercel.app/",
+    github_link: "https://github.com/curator69/meals",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Ecommerce Dashboard",
+    description: "Ecommerce Dashboard",
+    demo_link: "https://charming-halva-a40377.netlify.app/",
+    github_link: "https://github.com/curator69/dashboard",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Omnifood",
+    description: "Your only food delivering app(healthy)",
+    demo_link: "https://omnifood-curator.netlify.app/",
+    github_link: "https://github.com/curator69/omnifood-curator",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Natours",
+    description: "Your only Tours booking app",
+    demo_link: "https://curator69.github.io/natours-frontend/",
+    github_link: "https://github.com/curator69/natours-frontend",
+  },
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Youtube Clone",
+    description: "",
+    demo_link: "https://yt-clone-curator.netlify.app/",
+    github_link: "https://github.com/curator69/youtube-clone",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Space Travel",
+    description: "",
+    demo_link: "https://curator69.github.io/space-travel-site/",
+    github_link: "https://github.com/curator69/space-travel-site",
+  },
+];
