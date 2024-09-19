7a5d3f0a94c18f1c8fc8a18b53ae1cbd6dd12e7e
Rushikesh <rushikeshkhillari10@gmail.com>
2024-09-19 23:00:55 +0530

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,14 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   assetsInclude: ["**/*.glb"],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.glb"],
-  server: {
-    historyApiFallback: true, // Redirects all 404 requests to index.html
-  },
+  assetsInclude: ["*/.glb"],
+  base: "/",
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {}, // Keep this option if you plan on adding manual chunking
+        manualChunks: {},
       },
     },
   },
